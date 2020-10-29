adcb005b74ef366b95a4994d2ec3d192582e42df
breakfastitem <andrew.ehrman@gmail.com>
2020-10-29 19:36:47 -0400

    moved initial content data into object startcontent

--- script.js
+++ script.js
@@ -1,39 +1,59 @@
 /**
  * Declare objects in base html
  */
+console.log(window);
+var contentBox = document.getElementById("dynamic-content");
 
- var contentBox = document.getElementById("dynamic-content");
+var timer = document.getElementById("timer");
 
- var timer = document.getElementById("timer");
+/**
+* Create Initial content in grouped object
+*/
 
- initializeStartBox();
+var startContent = {
+    //properties hold html nodes
+    startHeader: window.document.createElement("h2"),
 
+    startInstructions: window.document.createElement("p"),
 
- function initializeStartBox(){
-    /**
-     * Create Initial content
-     */
-
-    var startHeader = document.createElement("h2");
-
-    var startInstructions =document.createElement("p");
-
-    var startButton =document.createElement("button");
+    startButton: window.document.createElement("button"),
 
     /**
-     * Initialize object content and place content on dom
-     */
+   * Initialize object content and place content on dom
+       */
 
-    startButton.textContent="Start";
-    startInstructions.textContent="Try to answer the following questions within the time limit.";
-    startHeader.textContent="Coding Quiz Challenge";
+    initializeStartBox: function () {
 
-    contentBox.appendChild(startHeader);
-    contentBox.appendChild(startInstructions);
-    contentBox.appendChild(startButton);
+        //startButton.setAttribute("class","btn");
 
- }
 
+        this.startButton.textContent = "Start";
+        this.startInstructions.textContent = "Try to answer the following questions within the time limit.";
+        this.startHeader.textContent = "Coding Quiz Challenge";
+
+        contentBox.appendChild(this.startHeader);
+        contentBox.appendChild(this.startInstructions);
+        contentBox.appendChild(this.startButton);
+
+    }
+
+
+
+};
+
+
+startContent.initializeStartBox();
+
+//startButton.addEventListener("click",startQuiz)
+
+
+
+/**
+ * Starts Timer and question mode
+ */
+function startQuiz() {
+
+}
 
 
 
