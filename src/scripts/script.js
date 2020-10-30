1a13f9d8937be417a27219cba8abe375a77ac786
breakfastitem <andrew.ehrman@gmail.com>
2020-10-30 05:08:53 -0400

    Added default question template, differentiated initializing a template to displaying a template.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,5 @@
 /**
- * link vars to html objects
+ * link vars to existing html elements in DOM
  */
 
 var contentBox = document.getElementById("dynamic-content");
@@ -12,6 +12,8 @@ var timer = document.getElementById("timer");
 
 var startContent = {
     //properties hold html nodes
+    container : window.document.createElement("div"),
+
     header: window.document.createElement("h2"),
 
     instructions: window.document.createElement("p"),
@@ -31,15 +33,21 @@ var startContent = {
         this.instructions.textContent = "Try to answer the following questions within the time limit.";
         this.header.textContent = "Coding Quiz Challenge";
 
-        contentBox.appendChild(this.header);
-        contentBox.appendChild(this.instructions);
-        contentBox.appendChild(this.button);
+        this.container.appendChild(this.header);
+        this.container.appendChild(this.instructions);
+        this.container.appendChild(this.button);
 
+    },
+
+    displayContent :function () {
+        contentBox.appendChild(this.container);
     }
 
 };
 
 var endContent ={
+
+    container : window.document.createElement("div"),
 
     header: window.document.createElement("h2"),
 
@@ -61,19 +69,72 @@ var endContent ={
         this.header.textContent = "All Done!";
         this.initialsInstructions.textContent ="Enter Initials:";
 
-        contentBox.appendChild(this.header);
-        contentBox.appendChild(this.score);
-        contentBox.appendChild(this.initialsForm);
+        this.container.appendChild(this.header);
+        this.container.appendChild(this.score);
+        this.container.appendChild(this.initialsForm);
         
         this.initialsForm.appendChild(this.initialsInstructions);
         this.initialsForm.appendChild(this.initialsInput);
         this.initialsForm.appendChild(this.initialsButton);
         
 
+    },
+
+    displayContent : function(){
+        contentBox.appendChild(this.container);
     }
+    
 
 
 };
+
+var questionContent = {
+
+    container : window.document.createElement("div"),
+
+    questionText : window.document.createElement("p"),
+
+    answerList : window.document.createElement("ul"),
+
+    //unInitialized until initialize method is called
+    answerListItems : [ , , , ] ,
+
+    answerListButtons : [ , , , ] ,
+
+    correctAnswerIndex : -1,
+
+    
+    initializeContent : function () {
+
+        //TODO:: get from method and object
+        this.questionText.textContent = "what is up?";
+        correctAnswerIndex=2;
+
+        //initialize button list
+        for(var i= 0;i<4;i++){
+
+            this.answerListItems[i]=window.document.createElement("li");
+            this.answerListButtons[i]= window.document.createElement("button");
+
+            this.answerListButtons[i].textContent="Answer "+i;
+
+            this.answerList.appendChild(this.answerListItems[i]);
+            this.answerListItems[i].appendChild(this.answerListButtons[i]);
+        }
+    
+
+        this.container.appendChild(this.questionText);
+        this.container.appendChild(this.answerList);
+        
+
+    },
+
+    displayContent : function(){
+        contentBox.appendChild(this.container);
+    }
+
+
+}
 
 /**
  * Starts Timer and question mode
@@ -84,8 +145,12 @@ function startQuiz() {
 
 
 endContent.initializeContent();
+startContent.initializeContent();
+questionContent.initializeContent();
 
-startContent.button.addEventListener("click",startQuiz)
+startContent.button.addEventListener("click",startQuiz);
+
+questionContent.displayContent();
 
 
 
